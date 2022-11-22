b9abf4952e27e79e1cbb0dd0fdffa27aa8dc3821
Dmytro Polutov <polutov@MacBook-Pro-16.local>
2022-11-22 09:48:14 +0200

    Added and styled registry form

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('signup', signup),
+    path('email_confirm', email_confirm),
+    path('signin', signin),
+    path('signout', signout),
+    path('profile', profile),
+    path('pass_reset', pass_reset),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'account/signup.html')
+    elif request.method == 'POST':
+
+        return render(request, 'account/signup_report.html')
+
+
+def email_confirm(request):
+    return render(request, 'account/email_confirm.html')
+
+
+def signin(request):
+    return render(request, 'account/signin.html')
+
+
+def signout(request):
+    return render(request, 'account/signout.html')
+
+
+def profile(request):
+    return render(request, 'account/profile.html')
+
+
+def pass_reset(request):
+    return render(request, 'account/pass_reset.html')
