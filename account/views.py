b8911b8803e003af1c490e12e89897acd4beaa7e
Dmytro Polutov <polutov@MacBook-Pro-16.local>
2022-11-22 13:07:21 +0200

    Added user registration prototype code

--- account/views.py
+++ account/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from hashlib import md5
 
 
 def signup(request):
     if request.method == 'GET':
         return render(request, 'account/signup.html')
     elif request.method == 'POST':
+        login = request.POST.get('login')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass_hash = md5(pass1.encode('utf-8')).hexdigest()
+        
+        report = dict()
+        new_user = User.objects.create_user(login, email, pass_hash)
+        if new_user is None:
+            report['message'] = 'У реєстрації відмовлено!'
+        else:
+            report['message'] = 'Ви успішно зареєстровані!'
 
-        return render(request, 'account/signup_report.html')
+        return render(request, 'account/signup_report.html', context=report)
 
 
 def email_confirm(request):
